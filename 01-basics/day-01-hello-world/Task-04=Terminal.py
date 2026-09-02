c98f79f8c8ecd4d58abff5db4664647014b574f3
Mahfoz <mdmahfoz.codes@gmail.com>
2026-09-02 18:02:04 +0600

    Task-04=Terminal

---
+++ 01-basics/day-01-hello-world/Task-04=Terminal.py
@@ -0,0 +1,22 @@
+print("=======================================")
+print("           PYTHON TERMINAL v1.0        ")
+print("=======================================")
+
+
+print()
+
+print("> Starting Python... ")
+print("> Loading files...")
+print("> cheking system...")
+print("> Python installed successfully!")
+print("> Starting learning journey...")
+
+print()
+
+print("STATUS: SUCCESS")
+
+print()
+
+print("==========================================")
+print("            Welcome, Programmer!"          )
+print("==========================================")
