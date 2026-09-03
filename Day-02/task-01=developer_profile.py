652677d8a19dce3cd4fc905f3facade2de38297a
Mahfoz <mdmahfoz.codes@gmail.com>
2026-09-03 13:33:54 +0600

    Task-01=Devoloper_profile

--- Day-02/task-01=developer_profile.py
+++ Day-02/task-01=developer_profile.py
@@ -1,4 +1,22 @@
+# These are variables
 Name = "Mahfoz"
 Age = "19"
 Country = "Bangladesh"
-Goal ="Softwere engnee
+Goal = "Software engineer"
+Programming_Language ="Python"
+Learning_hours ="5hrs"
+Favorite_editor = "Vs Code"
+Experience_level= "Beginner"
+
+# Output of my Variables
+print(f"My name is {Name}")
+print(f"i'm {Age} years old")
+print(f"I live in {Country}")
+print(f"My Goal is want to be a {Goal}")
+print(f"I'm Learning a high-level programming language called {Programming_Language}")
+print(f"My daily Learning hours is {Learning_hours}")
+print(f"currently i'm using {Favorite_editor} to learn {Programming_Language}")
+print(f"currently i'm a {Experience_level} but if i try hard i will improve my skill")
+
+print("--------STEP BY STEP--------")
+print("-----I'M GOING CLOSER TO MY GOAL")
