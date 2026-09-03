ec230d472405e889083b8779e818b8a3735ba074
Mahfoz <mdmahfoz.codes@gmail.com>
2026-09-03 23:40:54 +0600

    Task-02= CHARATER STATUS

---
+++ Day-02/task-03=Laptop_specification.py
@@ -0,0 +1,28 @@
+# Those Variable are useful for my project 
+Brand = "Lenovo"
+Model = "Ideapad Slim 5"
+Price = "$850"
+Ram = "16 GB"
+Storage = "512 GB SSD"
+Processor = "AMD Ryzen 7"
+Screen_size = "15.6 inches"
+Battery = "10 hours"
+Weight = "1.7 kg"
+Year = 2025
+# Project Star from here
+
+print("\n" 
+"--------------------------------------------------------------------------\n"
+"                     LAPTOP SPECIFICATION IN 2026\n"
+"--------------------------------------------------------------------------\n"
+"The laptop market size has grown strongly in recent years. It will\n"
+"grow from $272.91 billion in 2025 to $293.14 billion in 2026 at a\n"
+"compound annual growth rate (CAGR) of 7.4%. The growth in the\n"
+"historic period can be attributed to growth in mobile computing\n"
+"needs, enterprise workforce digitization, expansion of education\n"
+"technology, decline of desktop usage, global internet penetration.\n"
+"One of the most favorite Laptop in 2026 specification are given down below\n"
+"---------------------------------------------------------------------------\n"
+"                    LAPTOP SPECIFICATION\n"
+"----------------------------------------------------------------------------\n"
+# "Brand "f"{Brand}\n)
