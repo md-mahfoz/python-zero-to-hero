2bbc4d6ad17c84d0457b414155682519e6571ee7
Mahfoz <mdmahfoz.codes@gmail.com>
2026-09-04 22:43:23 +0600

    Rectangle Perimeter Calculation

--- Variable Math Problem/task 1 = rectangle Area calc.py
+++ Variable Math Problem/task 1 = rectangle Area calc.py
@@ -3,3 +3,5 @@ width = float(input("Enter the width: "))
 area = length * width
 
 print(f"The area is: {area}")
+
+
---
+++ Variable Math Problem/task-2=Recrangle Perimeter.py
@@ -0,0 +1,6 @@
+# Task 2 - Rectangle Perimeter
+Length = float(input("Enter the Length: "))
+Width = float(input("Enter the Width:"))
+area = 2 * (Length + Width)
+
+print(f"the Area is: {area}")
